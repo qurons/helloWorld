56c1bbcc9cccacbff5f9f92ed6ae75fbd55bf5c0
aktserver <aktserver@gmail.com>
2022-05-30 20:06:21 +0500

    first commit

---
+++ example/NewCode.java
@@ -0,0 +1,13 @@
+package example;
+
+public class NewCode {
+    public static void main(String[] args) {
+        System.out.println("BePro");
+        String make = "Dert";
+        int a = 5;
+        int t = 9;
+        System.out.println();
+        System.out.println(a+t);
+
+    }
+}
